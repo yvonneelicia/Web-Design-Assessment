52b485854e0c2e5b7c4145b3425754741b4bab4c
yvonneelicia <100697098+yvonneelicia@users.noreply.github.com>
2022-04-23 23:42:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 
-    function ValidateForm() {
-        var name = document.forms["RegForm"]["Name"];
-        var email = document.forms["RegForm"]["Email"];
-        var phone = document.forms["RegForm"]["Phone"];
-        var message = document.forms["RegForm"]["Message"];
-  
-        if (name.value == "") {
-            window.alert("Please enter your name.");
-            name.focus();
-            return false;
-        }
-  
-        if (message.value == "") {
-            window.alert("Please enter your message.");
-            address.focus();
-            return false;
-        }
-  
-        if (email.value == "") {
-            window.alert(
-              "Please enter a valid e-mail address.");
-            email.focus();
-            return false;
-        }
-  
-        if (phone.value == "") {
-            window.alert(
-              "Please enter your phone number.");
-            phone.focus();
-            return false;
-        }
+    function validateContact(){
+      var name = document.getElementById("name").value;
+      var phone = document.getElementById("phone").value;
+      var email = document.getElementById("email").value;
+      var message = document.getElementById("message").value;
+      var errormsg = document.getElementById("errormsg");
+      
+      errormsg.style.padding = "10px";
 
+      var text;
+      if(name.length == 0){
+        text = "Please enter valid name";
+        errormsg.innerHTML = text;
+        return false;
+      }
+      if(isNaN(phone) || phone.length != 10){
+        text = "Please enter valid phone number (10 digits)";
+        errormsg.innerHTML = text;
+        return false;
+      }
+      if(email.indexOf("@") == -1 || email.length < 6){
+        text = "Please enter valid email";
+        errormsg.innerHTML = text;
+        return false;
+      }
+      if(message.length <= 50){
+        text = "Please type a message with at least 50 characters";
+        errormsg.innerHTML = text;
+        return false;
+      }
+      alert("Submitted Successfully!");
+      return true;
     }
